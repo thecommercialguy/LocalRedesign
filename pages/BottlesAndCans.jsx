0d17655e9f180c014c4081e720186d22429de23f
“William” <williamg28@gmail.com>
2025-07-29 12:24:57 -0500

    Tap to exapnd for menu mobile

--- pages/BottlesAndCans.jsx
+++ pages/BottlesAndCans.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import DrinkMenu from '../assets/drink-menu.JPG'
 import { usePageAnimation } from '../src/hooks/usePageAnimation'
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
+import { ScreenWidthContext } from '../src/contexts/ScreenWidthContext'
+
+const styles = {
+    container: {
+
+    }
+}
 
 export default function BottlesAndCans() {
     const [isLoaded, setIsLoaded] = useState(false)
+    const [isTapped, setIsTapped] = useState(false)
+    const { windowWidth } = useContext(ScreenWidthContext)
+    const isMobile = windowWidth < 810
     const animation = usePageAnimation()
+
+    const handleTap = () => {
+        // Only allow tapping on mobile devices
+        if (isMobile) {
+            setIsTapped(!isTapped);
+        }
+    }
+
 
     return (
         <motion.div
@@ -16,16 +34,33 @@ export default function BottlesAndCans() {
             transition={animation.transition}
             className="bottles-and-drinks-container"
         >
-            <div className="image-container">
+            <motion.div
+                onTap={handleTap} 
+                className={`image-container ${isTapped && isMobile ? 'tapped' : ''}`}
+                layout 
+            >
                 {!isLoaded && <div className="skelton-loader drinks"></div>}
 
                 <img
                     onLoad={()=>{setIsLoaded(true)}}
                     src={DrinkMenu} 
                     alt="Drink menu"
+                    style={{display: isLoaded ? 'block' : 'none'}}
                 />
 
-            </div>
+            </motion.div>
+
+            <AnimatePresence>
+                {isTapped && isMobile && (
+                    <motion.div
+                        className="backdrop"
+                        onClick={() => setIsTapped(false)}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    />
+                )}
+            </AnimatePresence>
         </motion.div>
     )
 }
